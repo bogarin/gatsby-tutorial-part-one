84a611dd65d4215196041068fcecded726b60589
Jose Ramón Bogarin <bogarin617@gmail.com>
2018-09-06 19:01:54 -0700

    practica parte 2

---
+++ src/utils/typography.js
@@ -0,0 +1,11 @@
+import Typography from "typography";
+import bootstrapTheme from "typography-theme-bootstrap";
+import lawtonTheme from "typography-theme-lawton";
+import githubTheme from "typography-theme-github";
+
+//const typography = new Typography(bootstrapTheme);
+//const typography = new Typography(lawtonTheme);
+const typography = new Typography(githubTheme);
+//const typography = new Typography({ baseFontSize: "18px" });
+
+export default typography;
