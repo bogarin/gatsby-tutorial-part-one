16ed6697a18cbca29209ff4f3cb3acedea85e6d0
Jose Ramón Bogarin Valenzuela <bogarin617@gmail.com>
2018-09-17 01:52:55 -0700

    pasando al curso 4

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,11 +1,13 @@
 import Typography from "typography";
-import bootstrapTheme from "typography-theme-bootstrap";
-import lawtonTheme from "typography-theme-lawton";
-import githubTheme from "typography-theme-github";
+//import bootstrapTheme from "typography-theme-bootstrap";
+//import lawtonTheme from "typography-theme-lawton";
+//import githubTheme from "typography-theme-github";
+import fairyGateTheme from "typography-theme-fairy-gates";
 
+const typography = new Typography(fairyGateTheme);
 //const typography = new Typography(bootstrapTheme);
 //const typography = new Typography(lawtonTheme);
-const typography = new Typography(githubTheme);
+//const typography = new Typography(githubTheme);
 //const typography = new Typography({ baseFontSize: "18px" });
 
 export default typography;
